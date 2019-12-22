2e3d0c1dcedff685ad928091ab2afc105d898d33
Your Name <754857169@qq.com>
2019-12-22 22:49:12 +0800

    主函数修改

--- 计算机网络上机/main.cpp
+++ 计算机网络上机/main.cpp
@@ -7,13 +7,13 @@ int main()
 {
 	Graph net;
 	net.ReadFile();
-	cout << "已生成网络拓扑，请输入相应编号选择需要的操作" << endl;
 	cout << "1.增加路由器" << endl;
 	cout << "2.增加边" << endl;
 	cout << "3.删除路由器" << endl;
 	cout << "4.删除边" << endl;
 	cout << "5.输出路由表" << endl;
 	cout << "6.结束" << endl;
+	cout << "已生成网络拓扑，请输入相应编号选择需要的操作" << endl;
 	int choice = 0;
 	cin >> choice;
 	switch (choice)
